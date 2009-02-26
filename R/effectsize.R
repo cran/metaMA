0ca8b36750f9ee8ebc127d1dc4c86beca187d9de
Guillemette Marot <guillemette.marot@jouy.inra.fr>
2009-02-26 00:00:00 +0000

    version 1.1

---
+++ R/effectsize.R
@@ -0,0 +1,14 @@
+`effectsize` <-
+function(tstat,ntilde,m)
+{
+cm=gamma(m/2)/(sqrt(m/2)*gamma((m-1)/2))
+d=tstat/sqrt(ntilde)
+dprime=cm*d
+terme1=m/((m-2)*ntilde)
+vard=terme1+d^2*(terme1*ntilde-1/cm^2)
+vardprime=cm^2*(terme1+dprime^2*(terme1*ntilde-1/cm^2))
+result=cbind(d,vard,dprime,vardprime)
+colnames(result)=c("d","vard","dprime","vardprime")
+result
+}
+
